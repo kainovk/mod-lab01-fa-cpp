d0c6181b9fd646b8591f6e318443f6fab3b4232d
Kirill Kainov <kainov.dev@gmail.com>
2023-03-07 23:55:48 +0300

    lab1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,79 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cstring>
+#include <iostream>
+#include <math.h>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+using namespace std;
+
+
+unsigned int faStr1(const char* str) {
+    int count = 0;
+    int length = strlen(str);
+    bool isWord = false;
+
+    for (int i = 0; i < length; i++) {
+        char current = str[i];
+
+        if (current >= '0' && current <= '9') {
+            isWord = false;
+            break;
+        }
+        else {
+            isWord = true;
+        }
+        if ((current == ' ' || current == '\0') && isWord) {
+            count++;
+        }
+    }
+
+    return isWord ? count + 1 : count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    int count = 0;
+    bool wordStarted = false;
+    bool validWord = false;
+
+    while (*str != '\0') {
+        if (isupper(*str) && !wordStarted) {
+            wordStarted = true;
+        }
+        else if (*str == ' ' && wordStarted) {
+            count++;
+            wordStarted = false;
+        }
+        else if (!isalpha(*str) || !islower(*str)) {
+            wordStarted = false;
+        }
+        str++;
+    }
+
+    return wordStarted ? count + 1 : count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    int length = strlen(str);
+    int wordsCount = 0;
+    int sum = 0;
+    bool inWord = false;
+
+    for (int i = 0; i < length; i++) {
+        if (!inWord && str[i] != ' ') {
+            inWord = true;
+            wordsCount++;
+        }
+        else if (str[i] == ' ') {
+            inWord = false;
+        }
+        sum++;
+    }
+
+    if (wordsCount == 0) {
+        return 0;
+    }
+
+    double average = (double)sum / wordsCount;
+    int rounded = round(average);
+    return rounded;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+
+using namespace std;
+
 
 int main() {
-    return 0;
+    const char* str1 =
+        "This Is A Test String To Find Number of Words With Capital Letters";
+    unsigned int count1 = faStr1(str1);
+    cout << "faStr1: " << count1 << endl;
+
+    const char* str2 = "The quick Brown fox jumps over the lazy Dog";
+    unsigned int count2 = faStr2(str2);
+    cout << "faStr2: " << count2 << endl;
+
+    const char* str3 = "Hello, world! This is a test string.";
+    unsigned int averageLength = faStr3(str3);
+    cout << "faStr3: " << averageLength << endl;
 }
